fcd8d80beeffea39447bd51c9e159b223ec3eb81
Khuongnguyen27 <nguyenhuukhuong27102000@gmail.com>
2024-01-05 05:45:16 +0700

    export borrow user

---
+++ Modules/AdminExport/app/Exports/BorrowDevicesUserExport.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Modules\AdminExport\app\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Font;
+
+use App\Models\Borrow;
+use App\Models\User;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class BorrowDevicesUserExport {
+    protected $templateFile = '';
+    public $rules = [
+        'week' => 'required_without_all:school_years|nullable',
+        'school_years' => 'required_without_all:week|nullable',
+        'user_id' => 'required',
+    ];
+    public $messages = [
+        'week.required_without_all' => 'Trường tuần dạy là bắt buộc nếu không có năm dạy',
+        'school_years.required_without_all' => 'Trường năm dạy là bắt buộc nếu không có tuần dạy',
+        'user_id.required' => 'Trường tổ là bắt buộc',
+    ];
+    public function handle($request = null){
+        // $id = request()->id;
+        $type = request()->type;
+        // Lấy thông tin người dùng và mượn thiết bị
+        // $borrow = Borrow::find($id);
+        $user = User::find(request()->user_id);
+        $query = Borrow::query();
+        $query = $query->where('user_id', request()->user_id);
+        if (request()->week) {
+            $startWeek = Carbon::parse(request()->week)->startOfWeek()->format('Y-m-d');
+            $endWeek = Carbon::parse(request()->week)->endOfWeek()->format('Y-m-d');
+            $query = $query->whereBetween('borrow_date', [$startWeek, $endWeek]);
+        }
+        $borrows = $query->get();
+        // Đường dẫn đến mẫu Excel đã có sẵn
+        $templatePath = public_path('system/export/'.$type.'.xlsx');
+
+        // Tạo một Spreadsheet từ mẫu
+        $reader = IOFactory::createReader("Xlsx");
+        $spreadsheet = $reader->load($templatePath);
+
+        // Lấy sheet hiện tại
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('E2', $user->name ?? '');
+        $sheet->setCellValue('I2', $user->nest->name ?? '');
+        $sheet->setCellValue('L2', $user->nest->name ?? '');
+
+        // Duyệt qua danh sách mượn thiết bị
+        $index = 6; // Bắt đầu từ hàng 10
+        $stt = 1; // Khởi tạo biến STT
+        foreach ($borrows as $borrow) {
+            foreach ($borrow->the_devices as $device) {
+                $sheet->setCellValueExplicit('A' . $index, $stt, \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                $sheet->getStyle('A' . $index)->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_GENERAL);
+                $sheet->setCellValue('B' . $index, $device->borrow_date);
+                $sheet->setCellValue('C' . $index, $device->return_date);
+                $sheet->setCellValue('D' . $index, $borrow->id);
+                $sheet->setCellValue('E' . $index, $borrow->borrow_date);
+                $sheet->setCellValue('F' . $index, $device->device->name ?? '');
+                $sheet->setCellValue('G' . $index, $device->quantity);
+                $sheet->setCellValue('H' . $index, $device->tiet);
+                $sheet->setCellValue('I' . $index, $device->lesson_name);
+                $sheet->setCellValue('J' . $index, $device->room->name ?? '');
+                $sheet->setCellValue('K' . $index, $borrow->borrow_note ?? '');
+                $sheet->setCellValue('L' . $index, $user->name);
+                $index++;
+                $stt++;
+            }
+        }
+
+        $spreadsheet->setActiveSheetIndex(0);
+        $newFilePath = public_path('system/uploads/'.$type.$user->id.'.xlsx');
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($newFilePath);
+        return $newFilePath;
+    }
+}
--- Modules/AdminExport/resources/views/types/borrowdevicesnest.blade.php
+++ Modules/AdminExport/resources/views/types/borrowdevicesnest.blade.php
@@ -5,25 +5,19 @@
     <input type="week" min="2022-W01" max="{{ date('Y') }}-W99" name="week" class="form-control"
         value="{{ request()->week }}" onchange="this.form.submit()">
     <p class="mb-0">Nếu đã chọn Năm thì không chọn Tuần</p>
-    @error('week')
-    <p style="color:red">{{ $message }}</p>
-    @enderror
+    <x-admintheme::form-input-error field="week" />
 </div>
 <div class="form-group mb-4">
     <label class="form-label fw-bold">Ngày dạy : Năm</label>
     <x-admintheme::form-input-school-years name="school_years" selected_id="{{ request()->school_years }}"
         autoSubmit="true" />
     <p class="mb-0">Nếu đã chọn Tuần thì không chọn Năm</p>
-    @error('school_years')
-    <p style="color:red">{{ $message }}</p>
-    @enderror
+    <x-admintheme::form-input-error field="school_years" />
 </div>
 <div class="form-group mb-4">
     <label class="form-label fw-bold">Chọn Tổ</label>
     <x-admintheme::form-input-nests name="nest_id" selected_id="{{ request()->nest_id }}" autoSubmit="true" />
     <p class="mb-0">Không chọn có nghĩa là xuất cho tất cả các tổ</p>
-    @error('nest_id')
-    <p style="color:red">{{ $message }}</p>
-    @enderror
+    <x-admintheme::form-input-error field="nest_id" />
 </div>
 @endsection
