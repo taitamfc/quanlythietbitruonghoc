c620d560cbebdac934a3b04c5cd4e36f99d9353f
taifamfc <taitam8590@gmail.com>
2024-01-07 19:02:20 +0700

    fix env

--- Modules/AdminUser/app/Http/Controllers/AdminAuthController.php
+++ Modules/AdminUser/app/Http/Controllers/AdminAuthController.php
@@ -24,7 +24,7 @@ class AdminAuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->route('adminuser.index');
+            return redirect()->route('admin.home');
         }
     }
 
