c8deaf74486d575ec9672f8b15b3aab332bb8f04
Khuongnguyen27 <nguyenhuukhuong27102000@gmail.com>
2024-01-08 14:50:51 +0700

    feature export device

--- Modules/AdminExport/app/Exports/BorrowDevideExport.php
+++ Modules/AdminExport/app/Exports/BorrowDevideExport.php
@@ -23,65 +23,88 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
-class BorrowDetailExport {
+class BorrowDevideExport {
     protected $templateFile = '';
-    public function rules() : array{
+    public function rules(): array
+    {
         $rules = [
-            'id' => 'required|exists:borrows,id'
+            'week' => 'required',
+            'school_years' => 'required',
         ];
+        // nếu đã chọn school_years thì không yêu cầu week
+        if(request()->school_years){
+            unset($rules['week']);
+        }
+        // nếu đã chọn week thì không yêu cầu school_years
+        if(request()->week){
+            unset($rules['school_years']);
+        }
+        if(request()->school_years && request()->week){
+            unset($rules['school_years']);
+        }
         return $rules;
     }
     public $messages = [
         'required' => 'Trường yêu cầu',
-        'exists' => 'ID không tồn tại',
     ];
     public function handle($request = null){
-        $id = request()->id;
         $type = request()->type;
         // Lấy thông tin người dùng và mượn thiết bị
-        $borrow = Borrow::find($id);
-        $user = $borrow->user;
+        $query = \App\Models\Borrow::query();
 
         // Đường dẫn đến mẫu Excel đã có sẵn
         $templatePath = public_path('system/export/'.$type.'.xlsx');
 
+        if (request()->week && request()->school_years) {
+            $startDateEndDate = \App\Models\Borrow::getStartEndDateFromWeek(request()->week);
+            $startDateEndDate = array_values($startDateEndDate);
+            $query->whereBetween('borrow_date', $startDateEndDate);
+        }elseif( request()->week ){
+            $startDateEndDate = \App\Models\Borrow::getStartEndDateFromWeek(request()->week);
+            $startDateEndDate = array_values($startDateEndDate);
+            $query->whereBetween('borrow_date', $startDateEndDate);
+        }elseif(request()->school_years){
+            $startDateEndDate = \App\Models\Borrow::getStartEndDateFromYear(request()->school_years);
+            $startDateEndDate = array_values($startDateEndDate);
+            $query->whereBetween('borrow_date', $startDateEndDate);
+        }
+        $query->select('user_id');
+        $query->groupBy('user_id');
+        $items = $query->get();
         // Tạo một Spreadsheet từ mẫu
         $reader = IOFactory::createReader("Xlsx");
         $spreadsheet = $reader->load($templatePath);
-
-        // Lấy ngày, tháng và năm hiện tại
-        $currentDay = date('d');
-        $currentMonth = date('m');
-        $currentYear = date('Y');
-        $newValue = 'Gio Linh, ngày ' . $currentDay . ' tháng ' . $currentMonth . ' năm ' . $currentYear;
-       
+        
         // Lấy sheet hiện tại
+        $dateStart = date('d/m/Y',strtotime($startDateEndDate[0]));
+        $dateEnd = date('d/m/Y',strtotime($startDateEndDate[1]));
+        $date = Carbon::createFromFormat('d/m/Y', $dateEnd);
+        $year = $date->year;
+        
         $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('C7', $user->name ?? '');
-        $sheet->setCellValue('C8', $user->nest->name ?? '');
-        $sheet->setCellValue('D25', $user->name ?? '');
-        $sheet->setCellValue('C21', $newValue ?? '');
-        $sheet->setCellValue('C5', date('d/m/Y',strtotime($borrow->borrow_date)));
+        $sheet->setCellValue('D6',$dateStart);
+        $sheet->setCellValue('F6',$dateEnd);
+        $sheet->setCellValue('E7',$year);
 
         // Duyệt qua danh sách mượn thiết bị
         $index = 10; // Bắt đầu từ hàng 10
         $stt = 1; // Khởi tạo biến STT
-        foreach ($borrow->the_devices as $device) {
+        foreach ($items as $item) {
             $sheet->setCellValueExplicit('A' . $index, $stt, \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
             $sheet->getStyle('A' . $index)->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_GENERAL);
-            $sheet->setCellValue('B' . $index, $device->device->name ?? '');
-            $sheet->setCellValue('C' . $index, $device->lesson_name);
-            $sheet->setCellValue('D' . $index, Carbon::parse($borrow->borrow_date)->format('d/m/Y'));
-            $sheet->setCellValue('E' . $index, $device->lecture_name);
-            $sheet->setCellValue('F' . $index, $device->quantity);
-            $sheet->setCellValue('H' . $index, $device->room->name ?? '');
-            $sheet->setCellValue('G' . $index, $device->lecture_number);
+            $sheet->setCellValue('B' . $index, $item->user->name ?? '');
+            $sheet->setCellValue('C' . $index, $item->user->nest->name ?? '');
+            $sheet->setCellValue('D' . $index, '');
+            $sheet->setCellValue('E' . $index, '');
+            $sheet->setCellValue('F' . $index, '');
+            $sheet->setCellValue('G' . $index, '');
+            $sheet->setCellValue('H' . $index, '');
             $index++;
             $stt++;
         }
 
         $spreadsheet->setActiveSheetIndex(0);
-        $newFilePath = public_path('system/tmp/'.$type.$borrow->id.'.xlsx');
+        $newFilePath = public_path('system/tmp/'.$type.'.xlsx');
 
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
         $writer->save($newFilePath);
